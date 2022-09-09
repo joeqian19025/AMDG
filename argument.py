96d16839706bc66a4c5bf5a06af26dbc5db3308c
Joe Qian <xiaochi.qian@balliol.ox.ac.uk>
2022-09-09 20:58:47 +0100

    change default epoch number to 100

--- argument.py
+++ argument.py
@@ -24,7 +24,7 @@ parser.add_argument("--dataset_folder", type=str, default="../")
 parser.add_argument("--test_envs", nargs="+", type=int, default=[0])
 parser.add_argument("--train_val_ratio", type=float, default=0.9)
 
-parser.add_argument("--encoder_epochs", type=int, default=500)
+parser.add_argument("--encoder_epochs", type=int, default=100)
 parser.add_argument("--batchsize", type=int, default=64)
 parser.add_argument("--lr", type=float, default=0.001)
 parser.add_argument("--weight_decay", type=float, default=0.001)
